f6db9d922633428beb6160478be49269dc152f99
reyshaznii <reyshazni@gmail.com>
2024-03-18 11:36:19 +0700

    enhance: grouping endpoint

--- application/main.go
+++ application/main.go
@@ -5,11 +5,13 @@ import "github.com/gofiber/fiber/v3"
 func main() {
 	app := fiber.New()
 
-	app.Get("/", func(c fiber.Ctx) error {
+	queue := app.Group("/bc/queue")
+
+	queue.Get("/", func(c *fiber.Ctx) error {
 		return c.SendString("Hello, World 👋!")
 	})
 
-	app.Get("/ping", func(c fiber.Ctx) error {
+	queue.Get("/ping", func(c *fiber.Ctx) error {
 		return c.SendString("pong!")
 	})
 
