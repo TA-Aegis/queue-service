67e51982949c65b7982d95a9539c3575ababe945
reyshaznii <reyshazni@gmail.com>
2024-03-18 11:42:28 +0700

    fix: ctx usage for v3

--- application/main.go
+++ application/main.go
@@ -7,11 +7,11 @@ func main() {
 
 	queue := app.Group("/bc/queue")
 
-	queue.Get("/", func(c *fiber.Ctx) error {
+	queue.Get("/", func(c fiber.Ctx) error {
 		return c.SendString("Hello, World 👋!")
 	})
 
-	queue.Get("/ping", func(c *fiber.Ctx) error {
+	queue.Get("/ping", func(c fiber.Ctx) error {
 		return c.SendString("pong!")
 	})
 
